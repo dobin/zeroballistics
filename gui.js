91d8dd1c86c7d03189bff5c5ad129c8df9a03f16
Dobin <dobin@broken.ch>
2021-01-03 00:47:58 +0100

    refactor: code cleanup

--- gui.js
+++ gui.js
@@ -7,6 +7,9 @@ var UiConfig = function() {
     this.traj1 = false;
     this.traj2 = true;
     this.traj3 = false;
+    this.trajColor = [ 0xFF, 0x00, 0x00 ];
+    this.trajColorG = 0xFF0000;
+    this.show2ndDotTraj = false;
 
     this.scaleUi = 10;
     this.scale = 0.1;
@@ -36,9 +39,8 @@ function initGui() {
         width: 600,
         closed: false,
     });
-    //var guiLevel = gui.addFolder('defaults');
-    scaleCtrl = gui.add(uiConfig, 'scaleUi', 1, 100).name('Horicontal scale factor in percent').min(1).max(100);
-    scaleCtrl.onFinishChange(function(value) {
+    gui.remember(uiConfig);
+    gui.add(uiConfig, 'scaleUi', 1, 100).name('Horicontal scale factor in percent').min(1).max(100).onFinishChange(function(value) {
         value = value / 100;
         uiConfig.scale = value;
         resetGfx();
@@ -46,70 +48,55 @@ function initGui() {
 
     var f1 = gui.addFolder("Select trajectory based on zero distance and calibre")
 
-    uiConfig.traj0 = false;
-    traj0ctrl = f1.add(uiConfig, 'traj0').name(trajcetories[0].title);
-    traj0ctrl.onChange(function(value) {
+    f1.add(uiConfig, 'traj0').name(trajcetories[0].title).onChange(function(value) {
+        resetGfx();
+    }.bind(this));
+    f1.add(uiConfig, 'traj1').name(trajcetories[1].title).onChange(function(value) {
+        resetGfx();
+    }.bind(this));
+    f1.add(uiConfig, 'traj2').name(trajcetories[2].title).onChange(function(value) {
+        resetGfx();
+    }.bind(this));
+    f1.add(uiConfig, 'traj3').name(trajcetories[3].title).onChange(function(value) {
         resetGfx();
     }.bind(this));
 
-    uiConfig.traj1 = false;
-    traj1ctrl = f1.add(uiConfig, 'traj1').name(trajcetories[1].title);
-    traj1ctrl.onChange(function(value) {
-        resetGfx();
-    }.bind(this));
-
-    uiConfig.traj2 = true;
-    traj2ctrl = f1.add(uiConfig, 'traj2').name(trajcetories[2].title);
-    traj2ctrl.onChange(function(value) {
-        resetGfx();
-    }.bind(this));
-
-    uiConfig.traj3 = false;
-    traj3ctrl = f1.add(uiConfig, 'traj3').name(trajcetories[3].title);
-    traj3ctrl.onChange(function(value) {
+    f1.add(uiConfig, 'show2ndDotTraj').name("Also show interpolated trajectory of the 2nd dot").onChange(function(value) {
         resetGfx();
     }.bind(this));
 
     var f2 = gui.addFolder("Select optic options")
 
-    showLosCtrl = f2.add(uiConfig, 'showLos').name("Line Of Sight (LOS) indicator enabled?");
-    showLosCtrl.onChange(function(value) {
+    f2.add(uiConfig, 'showLos').name("Line Of Sight (LOS) indicator enabled?").onChange(function(value) {
         resetGfx();
     }.bind(this));
-    colorLosCtrl = f2.addColor(uiConfig, 'colorLos').name("Line Of Sight (LOS) color");
-    colorLosCtrl.onChange(function(value) {
+    f2.addColor(uiConfig, 'colorLos').name("Line Of Sight (LOS) color").onChange(function(value) {
         uiConfig.colorLosG = rgbToHex(uiConfig.colorLos[0], uiConfig.colorLos[1], uiConfig.colorLos[2]);
         resetGfx();
     }.bind(this));
 
-    showRingCtrl = f2.add(uiConfig, 'showRing').name("68MOA EOTECH reticle enabled?");
-    showRingCtrl.onChange(function(value) {
+    f2.add(uiConfig, 'showRing').name("68MOA Eotech reticle enabled?").onChange(function(value) {
         resetGfx();
     }.bind(this));
-    colorRingCtrl = f2.addColor(uiConfig, 'colorRing').name("68MOA EOTECH reticle color");
-    colorRingCtrl.onChange(function(value) {
+    f2.addColor(uiConfig, 'colorRing').name("68MOA Eotech reticle color").onChange(function(value) {
         uiConfig.colorRingG = rgbToHex(uiConfig.colorRing[0], uiConfig.colorRing[1], uiConfig.colorRing[2]);
         resetGfx();
     }.bind(this));
 
-    showTwoCtrl = f2.add(uiConfig, 'showTwo').name("EOTECH EXPS2-2 2nd dot (15MOA distance) enabled?");
-    showTwoCtrl.onChange(function(value) {
+    f2.add(uiConfig, 'showTwo').name("Eotech EXPS2-2 2nd dot (15MOA distance) enabled?").onChange(function(value) {
         resetGfx();
     }.bind(this));
-    colorTwoCtrl = f2.addColor(uiConfig, 'colorTwo').name("EOTECH EXPS2-2 2nd dot color");
-    colorTwoCtrl.onChange(function(value) {
+    f2.addColor(uiConfig, 'colorTwo').name("Eotech EXPS2-2 2nd dot color").onChange(function(value) {
         uiConfig.colorTwoG = rgbToHex(uiConfig.colorTwo[0], uiConfig.colorTwo[1], uiConfig.colorTwo[2]);
         resetGfx();
     }.bind(this));
-    topDotZeroCtrl = f2.add(uiConfig, 'topDotZero').name("EOTECH EXPS2-2 is center dot zeroed?");
-    topDotZeroCtrl.onChange(function(value) {
+    f2.add(uiConfig, 'topDotZero').name("Eotech EXPS2-2 is top dot zeroed?").onChange(function(value) {
         resetGfx();
     }.bind(this));
 
     var f2 = gui.addFolder("Other")
 
-    show10mCtrl = f2.add(uiConfig, 'show10m').name("Show enemy at 10m distance?");
-    show10mCtrl.onChange(function(value) {
+    f2.add(uiConfig, 'show10m').name("Show enemy at 10m distance?").onChange(function(value) {
         resetGfx();
     }.bind(this));
 }
